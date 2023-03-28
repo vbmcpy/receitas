660b601d21fb03bd78263f3c91bdb23346f0ab39
vbmcpy <vbmcpy@gmail.com>
2023-03-28 15:36:35 -0300

    urls

--- project/urls.py
+++ project/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
-from django.urls import path
+from django.urls import include, path
 
+# dominio o nome do APP
+#path('recipes/', include('recipes.urls')),
+
+# sem dominio
+#path('', include('recipes.urls')),
+
+path('recipes/', include('recipes.urls')),
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('recipes.urls')),
+    path('recipes/', include('recipes.urls')),
 ]
