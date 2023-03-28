f292ee512b65aa1185002439ef6d3368da367688
vbmcpy <vbmcpy@gmail.com>
2023-03-28 13:24:55 -0300

    allowed hosts *

--- project/settings.py
+++ project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b0kd-2mybfwd8k^9)to64@8nopxa%^6fugf6c^s2dn2iu01d80'
+SECRET_KEY = 'django-insecure-b0kd-2mybfwd8k ^ 9)to64@8nopxa % ^6fugf6c ^ s2dn2iu01d80'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
