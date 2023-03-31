928f2c821c7d8f536b913c3f0aaad4ddb5c48c50
vbmcpy <vbmcpy@gmail.com>
2023-03-31 16:15:26 -0300

    icone

--- recipes/views.py
+++ recipes/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 
 # namespace recipes para o template
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Victor Bruno'})  # noqa: E501
