3bacb2e7b59e815c24b077928327aac7c149eede
vbmcpy <vbmcpy@gmail.com>
2023-03-31 16:01:05 -0300

    removid sobre e contato

--- recipes/views.py
+++ recipes/views.py
@@ -5,11 +5,3 @@ from django.shortcuts import render
 # namespace recipes para o template
 def home(request):
     return render(request, 'recipes/home.html')
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
